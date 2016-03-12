ec3a704a709a876875f08fb74df199109fe23850
Peter Chen <peter@weme.sh>
2016-03-12 01:35:15 -0500

    Setup Pods linkage

---
+++ Trafolio/Trafolio-Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  Trafolio-Bridging-Header.h
+//  Trafolio
+//
+//  Created by Peter Chen on 2016-03-12.
+//  Copyright © 2016 CoreDev. All rights reserved.
+//
+
+#ifndef Trafolio_Bridging_Header_h
+
+#import <AFNetworking/AFNetworking.h>
+
+#define Trafolio_Bridging_Header_h
+
+
+#endif /* Trafolio_Bridging_Header_h */
